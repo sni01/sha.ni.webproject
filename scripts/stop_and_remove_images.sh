29e759a49e4b650ba06f2bc7429eca30184364d9
sni01 <likenisha2014@gmail.com>
2016-06-02 13:33:55 -0400

    aws_code_deploy_configure_files

--- scripts/stop_and_remove_images.sh
+++ scripts/stop_and_remove_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pre_containers = $(docker ps -a -q);
-if [$pre_containers == "" || $pre_containers == null] exit 0
-docker stop "$pre_containers";
+if [ "${pre_containers}" == "" ];
+    then exit 0
+docker stop "$pre_containers"
 docker rm "$pre_containers"
