a61cc3308251af6f7a92f9d1884ad8e8704f22ed
sni01 <likenisha2014@gmail.com>
2016-06-02 12:33:19 -0400

    aws_code_deploy_configure_files

--- scripts/stop_and_remove_images.sh
+++ scripts/stop_and_remove_images.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-if[$(docker ps -a -q)]
-    then
-        docker stop $(docker ps -a -q)
-        docker rm $(docker ps -a -q)
-fi
+docker stop "$(docker ps -a -q)"
+docker rm "$(docker ps -a -q)"
